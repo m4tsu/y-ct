98a1036c5dbc703065fd17bb8bdacef1b83cce29
m4tsu <matsurec0147@gmail.com>
2024-02-10 21:59:40 +0900

    chore: Setup for linters and run lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,88 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2022: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "prettier",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+  plugins: [
+    "react-hooks",
+    "react",
+    "react-refresh",
+    "jsx-a11y",
+    "@typescript-eslint",
+    "import",
+    "unused-imports",
+    "no-relative-import-paths",
+  ],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    "react/prop-types": "off",
+    "no-relative-import-paths/no-relative-import-paths": [
+      "error",
+      {
+        allowSameFolder: true,
+        rootDir: "src",
+        prefix: "@",
+      },
+    ],
+    "sort-imports": 0,
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "parent",
+          "sibling",
+          "index",
+          "object",
+          "type",
+        ],
+        pathGroups: [
+          {
+            pattern: "@/**",
+            group: "parent",
+            position: "before",
+          },
+        ],
+        alphabetize: {
+          order: "asc",
+        },
+        "newlines-between": "always",
+      },
+    ],
+    "@typescript-eslint/consistent-type-imports": [
+      "error",
+      {
+        prefer: "type-imports",
+      },
+    ],
+    "@typescript-eslint/strict-boolean-expressions": [
+      "error",
+      { allowString: false, allowNumber: false, allowNullableObject: false },
+    ],
+    "@typescript-eslint/switch-exhaustiveness-check": "error",
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-no-leaked-render": [
+      "error",
+      {
+        validStrategies: ["ternary"],
+      },
     ],
   },
-}
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
