bc5de4ea8a33d9556f3602ed31b6f329b1deb111
m4tsu <matsurec0147@gmail.com>
2024-02-12 00:36:08 +0900

    feat: Implemant hook for population composition view

--- src/pages/prefecture-population-composition/queries.ts
+++ src/pages/prefecture-population-composition/queries.ts
@@ -3,9 +3,11 @@ import {
   useQueries,
   useSuspenseQuery,
 } from '@tanstack/react-query';
+import { useMemo } from 'react';
 
 import { getpopulationComposition } from '@/api/prefectures/population/requests';
 import { getPrefectures } from '@/api/prefectures/requests';
+import type { PopulationComposition } from '@/domains/Population';
 import type { Prefecture } from '@/domains/Prefecture';
 
 const prefecturesQuery = queryOptions({
@@ -17,21 +19,72 @@ export const usePrefectures = () => {
   return useSuspenseQuery(prefecturesQuery);
 };
 
-const populationCompositionQuery = ({
-  prefCode,
-}: {
-  prefCode: Prefecture['prefCode'];
-}) =>
+const populationCompositionQuery = ({ prefCode, prefName }: Prefecture) =>
   queryOptions({
     queryKey: ['populationComposition', { prefCode }],
     queryFn: () => getpopulationComposition({ prefCode, cityCode: '-' }),
+    select: (data) => ({
+      prefCode,
+      prefName,
+      populationCompositions: data.data,
+    }),
   });
-export const usePopulationCompositions = (
-  prefCodes: Prefecture['prefCode'][],
-) => {
+export const usePopulationCompositionsQuery = (prefectures: Prefecture[]) => {
   return useQueries({
-    queries: prefCodes.map((prefCode) =>
-      populationCompositionQuery({ prefCode }),
+    queries: prefectures.map((prefecture) =>
+      populationCompositionQuery(prefecture),
     ),
   });
+};
+
+type PrefectureWithPopulationCompositions = Prefecture & {
+  compositionLabel: string;
+  populationCompositions: PopulationComposition[];
+};
+type Filter = {
+  compositionLabel?: string;
+};
+export const usePrefectureWithPopulationCompositions = (
+  prefectures: Prefecture[],
+  filter: Filter,
+) => {
+  const results = usePopulationCompositionsQuery(prefectures);
+  const isLoading = results.some((result) => result.isLoading);
+
+  const compositionLabels = useMemo(() => {
+    return (
+      results[0]?.data?.populationCompositions.map(
+        (composition) => composition.label,
+      ) ?? []
+    );
+  }, [results]);
+  const prefectureWithPopulationCompositions: PrefectureWithPopulationCompositions[] =
+    useMemo(() => {
+      return results.flatMap((result) => {
+        if (result.data !== undefined) {
+          const filterLabel = filter.compositionLabel ?? compositionLabels[0];
+          if (filterLabel === undefined) {
+            // data がある場合 compositionLabels は空にならない想定
+            throw new Error('compositionLabels is empty');
+          }
+          const compositions = result.data.populationCompositions.find(
+            (populationComposition) =>
+              populationComposition.label === filterLabel,
+          );
+          if (compositions === undefined) {
+            return [];
+          }
+          return {
+            prefCode: result.data.prefCode,
+            prefName: result.data.prefName,
+            compositionLabel: filterLabel,
+            populationCompositions: compositions.data,
+          };
+        } else {
+          return [];
+        }
+      });
+    }, [results, filter, compositionLabels]);
+
+  return { isLoading, prefectureWithPopulationCompositions, compositionLabels };
 };
